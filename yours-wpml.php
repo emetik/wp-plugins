c9b76e5b54f7dc3e3d23a148764fea321e93f3a2
Laurent Snackaert <emetik@gmail.com>
2017-02-07 12:02:35 +0100

    Second

--- yours-wpml.php
+++ yours-wpml.php
@@ -23,6 +23,8 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       yours-wpml
  * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/emetik/wp-plugins/
+ * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
